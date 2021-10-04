62bbf5436d2b71bfd1e5acc56d994415da1f6cfd
Volodia99 <gaylor.wafflard@univ-grenoble-alpes.fr>
2021-10-04 23:43:04 +0200

    cblind with some plotting tests - suite

--- cblind/__init__.py
+++ cblind/__init__.py
@@ -1,3 +1,3 @@
 from .__version__ import __version__
-from .cblind import Colorplots, cbmap, mapping, reversed_cmap
+from .cblind import Colorplots, cbmap, mapping, reversed_cmap, test_cblind, test_huescale, test_rainbow, test_rbscale, test_monocolor, test_mapping
 
