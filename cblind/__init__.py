6388027d81a084105006f2c1e4fa381b9d543a18
Volodia99 <gaylor.wafflard@univ-grenoble-alpes.fr>
2021-11-30 11:56:27 +0100

    changed call of functions in __ini__

--- cblind/__init__.py
+++ cblind/__init__.py
@@ -1,3 +1,3 @@
 from .__version__ import __version__
-from .cblind import Colorplots, cbmap, mapping, reversed_cmap, test_cblind, test_huescale, test_rainbow, test_rbscale, test_monocolor, test_mapping
+from .cblind import Colorplots, cbmap, mapping, reversed_cmap, test_cblind, test_contrast, test_huescale, test_solstice, test_bird, test_pregunta, test_extreme_rainbow, test_rainbow, test_rbscale, test_monocolor, test_mapping
 
